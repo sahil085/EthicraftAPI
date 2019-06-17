6465526b495a2532ec3a90fb07f341c750ae12d4
sahil <vermasahil269@gmail.com>
2019-06-17 09:55:39 +0530

    first commit

---
+++ src/main/java/com/iskcon/EthicraftAPI/EthicraftApiApplication.java
@@ -0,0 +1,13 @@
+package com.iskcon.EthicraftAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EthicraftApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EthicraftApiApplication.class, args);
+	}
+
+}

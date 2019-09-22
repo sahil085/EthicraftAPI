177e1a108b55388d26caa9d799c1fc40314f0c56
sahil <vermasahil269@gmail.com>
2019-09-22 05:53:51 +0530

    jwt security added

---
+++ src/main/java/com/iskcon/EthicraftAPI/securityConfig/jwt/JwtUserDetailsService.java
@@ -0,0 +1,31 @@
+
+package com.iskcon.EthicraftAPI.securityConfig.jwt;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.iskcon.EthicraftAPI.domain.User;
+import com.iskcon.EthicraftAPI.repository.UserRepository;
+
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user =  userRepository.findByEmail(username);
+		if (user == null) {
+			throw new UsernameNotFoundException("User not found with username: " + username);
+		}
+		return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(),
+				user.getAuthorities());
+	}
+
+
+}

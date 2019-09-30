5776e0fb299241e696f5034942c3c403bc60a0ac
sahil <vermasahil269@gmail.com>
2019-09-30 11:13:09 +0530

    code refactor in college controller

--- src/main/java/com/iskcon/EthicraftAPI/controller/CollegeController.java
+++ src/main/java/com/iskcon/EthicraftAPI/controller/CollegeController.java
@@ -1,6 +1,5 @@
 package com.iskcon.EthicraftAPI.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -17,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.iskcon.EthicraftAPI.co.CollegeCO;
-import com.iskcon.EthicraftAPI.co.CollegeReferenceCO;
 import com.iskcon.EthicraftAPI.constants.RoleConstant;
 import com.iskcon.EthicraftAPI.domain.College;
 import com.iskcon.EthicraftAPI.dto.CollegeDTO;
@@ -45,13 +43,6 @@ public class CollegeController {
     @PreAuthorize("hasAuthority('"+ RoleConstant.ROLE_ADMIN+"')")
     public ResponseDTO registerCollege(@RequestBody CollegeCO collegeCO)
     {
-        List<CollegeReferenceCO> collegeReferenceCOList = new ArrayList<>();
-        CollegeReferenceCO collegeReferenceCO = new CollegeReferenceCO();
-        collegeReferenceCO.setContact(9999998999l);
-        collegeReferenceCO.setDesignation("aasd");
-        collegeReferenceCO.setName("asda");
-                collegeReferenceCOList.add(collegeReferenceCO);
-        collegeCO.setReferenceList(collegeReferenceCOList);
         return collegeService.register(collegeCO);
     }
 

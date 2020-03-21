68f5f3d046e85041fe3f9271385ad9675ba7d3f3
sahil <vermasahil269@gmail.com>
2020-03-21 17:16:34 +0530

    login system change

--- src/main/java/com/iskcon/EthicraftAPI/securityConfig/jwt/JwtAuthenticationController.java
+++ src/main/java/com/iskcon/EthicraftAPI/securityConfig/jwt/JwtAuthenticationController.java
@@ -1,11 +1,15 @@
 package com.iskcon.EthicraftAPI.securityConfig.jwt;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,15 +32,15 @@ public class JwtAuthenticationController {
     private JwtUserDetailsService userDetailsService;
 
     @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
-    public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
+    public ResponseEntity<JwtResponse> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
 
         authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
 
         final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
 
         final String token = jwtTokenUtil.generateToken(userDetails);
-
-        return ResponseEntity.ok(new JwtResponse(token));
+        List<String> authorities = userDetails.getAuthorities().stream().map(o -> ((GrantedAuthority) o).getAuthority()).collect(Collectors.toList());
+        return ResponseEntity.ok(new JwtResponse(token, authorities,userDetails.getUsername()));
     }
 
     private void authenticate(String username, String password) throws Exception {
--- src/main/java/com/iskcon/EthicraftAPI/securityConfig/jwt/JwtResponse.java
+++ src/main/java/com/iskcon/EthicraftAPI/securityConfig/jwt/JwtResponse.java
@@ -1,16 +1,31 @@
 package com.iskcon.EthicraftAPI.securityConfig.jwt;
 
 import java.io.Serializable;
+import java.util.List;
+
 
 public class JwtResponse implements Serializable {
-    private static final long   serialVersionUID = -8091879091924046844L;
-    private final        String jwttoken;
+    private static final long         serialVersionUID = -8091879091924046844L;
+    private              String       jwtToken;
+    private              String       userName;
+    private              List<String> authorities;
 
-    public JwtResponse(String jwttoken) {
-        this.jwttoken = jwttoken;
+    public JwtResponse(String jwtToken, List<String> authorities, String userName) {
+        this.jwtToken = jwtToken;
+        this.authorities = authorities;
+        this.userName = userName;
     }
 
     public String getToken() {
-        return this.jwttoken;
+        return this.jwtToken;
     }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public List<String> getAuthorities() {
+        return authorities;
+    }
+
 }
